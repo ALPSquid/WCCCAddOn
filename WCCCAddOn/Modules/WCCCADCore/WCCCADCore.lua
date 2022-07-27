b579a8e6342e774aeceea3a1be006fdf729b9f84
ALPSquid <alpsquid@kolabnow.com>
2022-07-27 22:51:15 +0100

    Updated for 9.2 and promoted beta build to release

--- WCCCAddOn/Modules/WCCCADCore/WCCCADCore.lua
+++ WCCCAddOn/Modules/WCCCADCore/WCCCADCore.lua
@@ -6,10 +6,10 @@
 local _, ns = ...
 local WCCCAD = ns.WCCCAD
 
-WCCCAD.version = 1300
-WCCCAD.versionString = "1.3.0"
---WCCCAD.versionType = ns.consts.VERSION_TYPE.RELEASE
-WCCCAD.versionType = ns.consts.VERSION_TYPE.BETA
+WCCCAD.version = 1301
+WCCCAD.versionString = "1.3.1"
+WCCCAD.versionType = ns.consts.VERSION_TYPE.RELEASE
+--WCCCAD.versionType = ns.consts.VERSION_TYPE.BETA
 WCCCAD.newVersionAvailable = false
 
 
