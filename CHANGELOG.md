5beeb7791af89e0187074b9aae3ae6a37d137c71
ALPSquid <alpsquid@kolabnow.com>
2025-02-26 10:45:18 +0000

    Updated for 11.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# v1.7.6
+* Updated for 11.1.0
+
 # v1.7.5
 * Updated for 11.0.7
 
--- WCCCAddOn/Modules/WCCCADCore/WCCCADCore.lua
+++ WCCCAddOn/Modules/WCCCADCore/WCCCADCore.lua
@@ -6,8 +6,8 @@
 local _, ns = ...
 local WCCCAD = ns.WCCCAD
 
-WCCCAD.version = 1705
-WCCCAD.versionString = "1.7.5"
+WCCCAD.version = 1706
+WCCCAD.versionString = "1.7.6"
 WCCCAD.versionType = ns.consts.VERSION_TYPE.RELEASE
 --WCCCAD.versionType = ns.consts.VERSION_TYPE.BETA
 WCCCAD.newVersionAvailable = false
